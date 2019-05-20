524ea4ca827484b0dbe3d51125e2879f80827f21
DOMECQ <adrien.domecq@cgi.com>
2019-05-20 11:32:31 +0200

    tg julien

---
+++ src/main/java/com/example/models/Post.java
@@ -0,0 +1,52 @@
+package com.example.models;
+
+import java.awt.TextArea;
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Post {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	private String titre;
+	private Date date;
+	private TextArea contenu;
+	private int id_user;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitre() {
+		return titre;
+	}
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public TextArea getContenu() {
+		return contenu;
+	}
+	public void setContenu(TextArea contenu) {
+		this.contenu = contenu;
+	}
+	public int getId_user() {
+		return id_user;
+	}
+	public void setId_user(int id_user) {
+		this.id_user = id_user;
+	}
+}
